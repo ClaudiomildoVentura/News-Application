5ca06546255d7ac0ac5c2ee38f9e8cbca68911b3
Claudiomildo <claudiomildo@gmail.com>
2019-12-20 16:55:45 -0300

    restruturing folder

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-var express = require('express')
-var app = express()
+var app = require('./config/server')
 
-app.set('view engine', 'ejs')
+require('./app/routes/formNoticia')(app)
 
-app.get('/', function (req, res) {
-    res.render("home/index")
-})
-app.get('/formulario_inclusao_noticia', function (req, res) {
-    res.render("admin/form_add_noticia")
-})
-app.get('/noticias', function (req, res) {
-    res.render("noticias/noticias")
-})
-app.listen(3000, function () {
-    console.log('Server on start')
+require('./app/routes/noticias')(app)
+
+require('./app/routes/home')(app)
+
+app.listen(2000, function () {
+    console.log('server On!')
 })
