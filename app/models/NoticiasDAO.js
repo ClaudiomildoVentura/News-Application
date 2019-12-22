661882c79e10c2c17c1666fea0ebe540f9553186
Claudiomildo <claudiomildo@gmail.com>
2019-12-22 20:08:42 -0300

    Adjust final

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -8,7 +8,7 @@ try {
     }
 
     NoticiasDAO.prototype.getNoticia = function (id_noticia, callback) {
-        this._connection.query('SELECT * FROM noticias WHERE id_noticia = ' + id_noticia.id_noticia, callback)
+        this._connection.query('SELECT * FROM noticias WHERE id_noticia = ' + id_noticia.id_noticia, callback);
     }
 
     NoticiasDAO.prototype.salvarNoticia = function (noticia, callback) {
